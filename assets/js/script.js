358ad5fc1939dd24464198b4f3d25f8b10ea100c
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-06-27 10:29:37 +1000

    updated Js File

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-//Displying current date and time pulling from moment.js
+//Displying current date and time Using moment.js
 var dateTime = moment().format('dddd, MMMM Do - h:mm:ss a');
 $('#current').text(dateTime);
 // console.log(dateTime);
 //for each method to loop over each time block and display the colors
-function colorCoded() {
+function workDay() {
     var hourNow = moment().hour(); //displaying the current hour
     $(".time-block").each(function() {//targetting the class timeblock which is common in each div
         var Time = $(this).attr("id").split("-")[1];//It will return the string "hour-9" and soon.we will remove the dash using split()method and pull the index 1.
         console.log (Time);
-        //Adding and removing class past, present and future.
+        //Adding and Updating class past, present and future.
         if(Time < hourNow) {
             $(this).addClass("past");
             $(this).removeClass("present");
@@ -25,10 +25,10 @@ function colorCoded() {
             $(this).addClass("future");
         }   
     });}
-    colorCoded();
-    //Added event listener to the save button   
+    workDay();
+    //Event Listener assigned to Save Button   
     $(".saveBtn").on("click",function(){
-        var text = $(this).siblings(".description").val().trim();//grabbing the description class from sibling textarea html element 
+        var text = $(this).siblings(".des").val().trim();//grabbing the des class from sibling textarea html element 
         console.log(text);
         var value = $(this).parent().attr("id");
         console.log(value);
@@ -38,16 +38,16 @@ function colorCoded() {
     //Retrieving the stored data from local storage 
     //https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
     function displayData(){
-    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+    $("#hour-8 .des").val(localStorage.getItem("hour-8"));
+    $("#hour-9 .des").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .des").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .des").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .des").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .des").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .des").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .des").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .des").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .des").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .des").val(localStorage.getItem("hour-18"));
     };
     displayData();
