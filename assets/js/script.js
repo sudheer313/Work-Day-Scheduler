4442f6a46c00e1bcb66e5288d8d273efe853936b
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-07-11 08:34:00 +1000

    updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var dateTime = moment().format("MMM Do YY"); //https://momentjs.com/
 $("#current").html(dateTime);
 var time1 = document.querySelector(".time-block");
 var saveBtn = document.querySelectorAll("button");
-var textarea = $("textarea");
+//var textarea = $("textarea");
 var now = moment().hour();//current time
 var time = parseInt(time1.innerText.split("")[0]);//blocktime
 console.log(time)
@@ -10,12 +10,15 @@ console.log(now)
 function blockColor() {
     $('.time-block').each(function ()
     {
-        
+  var textarea = $(this).children('.textValue')
    if (time == now) {
-       textarea.css("background-color", "#75FF33");
-   } else {
-       textarea.css("background-color", "#FF5733");
-   }
-})
+      { textarea.addClass('present');}
+   } else if (time>now)
+    { textarea.addClass('future');
+}else
+{ textarea.addClass('past')};
+   })
 }
+
+
 blockColor();
