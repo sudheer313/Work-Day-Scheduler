8171f2753d125eb7803d77c9c6a264ec0e624d57
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-07-11 09:50:49 +1000

    updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,36 @@ var dateTime = moment().format("MMM Do YY"); //https://momentjs.com/
 $("#current").html(dateTime);
 var time1 = document.querySelector(".time-block");
 var saveBtn = document.querySelectorAll("button");
-//var textarea = $("textarea");
+
 var now = moment().hour();//current time
 var time = parseInt(time1.innerText.split("")[0]);//blocktime
 console.log(time)
 console.log(now)
 function blockColor() {
-    $('.time-block').each(function ()
-    {
-  var textarea = $(this).children('.textValue')
-   if (time == now) {
-      { textarea.addClass('present');}
-   } else if (time>now)
-    { textarea.addClass('future');
-}else
-{ textarea.addClass('past')};
-   })
+    // loop over time blocks
+    $('.time-block').each(function() {
+        //this will get the time in interger format from time-block
+        //referred from BCS help
+      var textarea = parseInt(
+        $(this)
+          .attr('id')
+          .split('-')[1]
+      );
+    if (textarea < now) {
+        $(this).addClass('past');
+        } else if (textarea === now) {
+        $(this).removeClass('past');
+         $(this).addClass('present');
+        } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+        }
+})
 }
-
-
 blockColor();
+$('.saveBtn').click(function (event)
+{
+console.log("testing");
+
+})
